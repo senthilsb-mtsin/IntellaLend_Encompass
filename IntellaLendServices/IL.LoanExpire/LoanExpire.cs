c1e235e08c14f95a7b5c56c6d136120d7c9af4a5
Mohan <mohan.s@mtsin.com>
2021-03-09 16:11:04 +0530

    Added new service(LoanExpire) to update Retention policy status
    Added new service(LoanExpire) to update Retention policy status

---
+++ IntellaLendServices/IL.LoanExpire/LoanExpire.cs
@@ -0,0 +1,44 @@
+﻿using MTS.ServiceBase;
+using MTSEntBlocks.ExceptionBlock.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL.LoanExpire
+{
+    public class LoanExpire : IMTSServiceBase
+    {
+        public void OnStart(string ServiceParam)
+        {
+
+        }
+
+        public bool DoTask()
+        {
+            try
+            {
+                UpdateRetentionPolicy();
+            }
+            catch (Exception ex)
+            {
+                MTSExceptionHandler.HandleException(ref ex);
+            }
+            return true;
+        }
+
+        private void UpdateRetentionPolicy()
+        {
+            LoanExpireDataAccess dataAccess = new LoanExpireDataAccess();
+            try
+            {
+                dataAccess.UpdateRetentionPolicyStatus();
+            }
+            catch (Exception ex)
+            {
+                MTSExceptionHandler.HandleException(ref ex);
+            }
+        }
+    }
+}
---
+++ IntellaLendServices/IL.LoanExpire/LoanExpireDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using MTSEntityDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL.LoanExpire
+{
+    public class LoanExpireDataAccess
+    {
+        #region Private Variables
+
+        private static string SystemSchema = "IL";
+
+        #endregion
+
+        #region Public Methods 
+
+        public void UpdateRetentionPolicyStatus()
+        {
+            using (var db = new DBConnect(SystemSchema))
+            {
+                db.Database.ExecuteSqlCommand($"EXEC [{SystemSchema}].[RETENTIONPOLICYSTATUSUPDATE]");
+            }
+        }
+    }
+
+    #endregion
+}
